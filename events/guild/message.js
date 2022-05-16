6ecd413fa0856c54629feab0d2a0f18012dffb6f
Nemesis <keziah.moselle@gmail.com>
2022-05-16 15:58:55 +0200

    fix: await on command.execute

--- events/guild/message.js
+++ events/guild/message.js
@@ -40,7 +40,7 @@ module.exports = async (client, message) => {
     if (!command) return message.reply("Quelque chose n'a pas tourné rond, bizarre. Soit votre commande n'existe pas, soit elle est erronée <:hmmmm:898672241787674634>");
 
     try {
-        command.execute(client, message, args, profileData);
+        await command.execute(client, message, args, profileData);
     } catch (err) {
         console.log(err);
     }
