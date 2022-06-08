a30be80b7bd48de845b4e621c130e94f198e4f26
amnezziaa <mathys.vst@gmail.com>
2022-06-08 16:55:05 +0200

    fix(commands): removed unused fonction

--- commands/recent.js
+++ commands/recent.js
@@ -7,18 +7,6 @@ function getValue(obj, key) {
     return obj[key];
 }
 
-function msToTime(duration) {
-    seconds = Math.floor((duration / 1000) % 60),
-        minutes = Math.floor((duration / (1000 * 60)) % 60),
-        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-    return hours + "h" + minutes + "m" + seconds + "s";
-}
-
 const rankTab = {
     XH: '<:XH:971030365772845156>',
     X: '<:X_:971030365839970304>',
