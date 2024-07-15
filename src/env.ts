6472006cadc154e27acea6b75cebba619aeaf06c
Mathysv <mathys.vst@gmail.com>
2024-07-15 14:29:05 +0200

    fix(env): remove deprecated zod property

--- src/env.ts
+++ src/env.ts
@@ -2,15 +2,15 @@ import 'dotenv/config';
 import z from 'zod';
 
 const envSchema = z.object({
-  DISCORD_BOT_ID: z.string().nonempty(),
-  DISCORD_TOKEN: z.string().nonempty(),
-  BOT_OWNER_ID: z.string().nonempty(),
+  DISCORD_BOT_ID: z.string(),
+  DISCORD_TOKEN: z.string(),
+  BOT_OWNER_ID: z.string(),
   BOT_NEEDED_PERMISSION_ID: z.string().transform(Number),
-  MONGODB_SRV: z.string().url().nonempty(),
+  MONGODB_SRV: z.string().url(),
   OSU_API_ID: z.string().transform(Number),
-  OSU_API_SECRET: z.string().nonempty(),
-  OSU_API_URL: z.string().url().nonempty(),
-  OSU_TOKEN_URL: z.string().url().nonempty()
+  OSU_API_SECRET: z.string(),
+  OSU_API_URL: z.string().url(),
+  OSU_TOKEN_URL: z.string().url()
 });
 
 export const ENV = envSchema.parse(process.env);
